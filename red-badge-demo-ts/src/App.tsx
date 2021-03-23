0ce5e45e4596804a6cbff7f0f286ca12b31469aa
Nate Bercovitz <natebercovitz@Nates-MacBook-Pro.local>
2021-03-23 17:48:02 -0600

    props & mapping demo

--- red-badge-demo-ts/src/App.tsx
+++ red-badge-demo-ts/src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import PropsExample from './components/PropsExample';
+import PropsMappingExample from './components/PropsMapping';
 
 function App() {
-  return <div></div>;
+  const visitedPlaces = ['Spain', 'Netherlands', 'Switzerland', 'Italy', 'Morocco', 'Canada']
+  return (
+  <div>
+    <PropsExample name="Tom" business="MySpace" />
+    <PropsMappingExample visited={visitedPlaces} />
+  </div>);
 }
 
 export default App;
+
