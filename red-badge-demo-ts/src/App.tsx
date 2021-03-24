198fde64d2c497495ff231fd6e8c8077df69fffd
Nate Bercovitz <natebercovitz@Nates-MacBook-Pro.local>
2021-03-23 18:53:16 -0600

    func to class challenge03

--- red-badge-demo-ts/src/App.tsx
+++ red-badge-demo-ts/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container } from "reactstrap";
-
 import FunctionalProps from "./components/FunctionalProps";
+import ClassComponentProps from "./components/ClassComponentProps";
 
 function App() {
   const numArray = [3, 5, 6, 7, 2, 6, 23, 5, 34];
@@ -15,6 +15,7 @@ function App() {
         </p>
       </Container>
       <FunctionalProps numbers={numArray} />
+      <ClassComponentProps numbers={numArray} />
     </div>
   );
 }
